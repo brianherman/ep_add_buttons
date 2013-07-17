a03e07d50c29d8afd5a676646edb7dbcfc7e023a
brian herman <brianherman@gmail.com>
2013-07-16 22:10:52 -0500

    added compilation.

--- handleMessage.js
+++ handleMessage.js
@@ -1,25 +1,32 @@
-var crypto = require('crypto'),
-padManager = require("ep_etherpad-lite/node/db/PadManager"),
-        fs = require('fs'),
+    crypto = require('crypto'),
+    padManager = require("ep_etherpad-lite/node/db/PadManager"),
+    fs = require('fs'),
     md5sum = crypto.createHash('md5');
 
+
 exports.handleMessage = function(hook_name, context, callback){
-  if (context.message && context.message.data){
-    if (context.message.data.type == 'WRITE_TO_FILESYSTEM' ) { // if it's a request to update an authors email
-      // Write padId data to filesystem
-      padManager.getPad(context.message.data.padId, null, function(err, value){
-        var hash = crypto.createHash('md5').update(context.message.data.padId).digest("hex");
-        fs.writeFile("/tmp/"+hash, value.atext.text, function(err) {
-          if(err){
-            console.err("failed to write text to hashed location");
-          }else{
-            console.log("Wrote pad contents to /tmp/"+hash);
-            // everything went great, we could tell the client here if we wanted..
-          }
-        });
-      });
-      callback([null]);
+    if (context.message && context.message.data){
+        if (context.message.data.type == 'WRITE_TO_FILESYSTEM' ) { // if it's a request to update an authors email
+            // Write padId data to filesystem
+            padManager.getPad(context.message.data.padId, null, function(err, value){
+                    var hash = crypto.createHash('md5').update(context.message.data.padId).digest("hex");
+                    fs.writeFile("~/"+hash+'.c', value.atext.text, function(err) {
+                        if(err){
+                        console.err("failed to write text to hashed location");
+                        }else{
+                        console.log("Wrote pad contents to /tmp/"+hash+'.c');
+                        // everything went great, we could tell the client here if we wanted..
+                        var child = require('child_process').exec('/usr/bin/emcc',
+                            ['/tmp/'+hash+'.c'], function(err, stdout, stderr) {
+                            // Node.js will invoke this callback when the
+                            console.log(stdout);
+                            });
+
+                        }
+                        });
+                    });
+            callback([null]);
+        }
     }
-  }
-  callback();
+    callback();
 }
